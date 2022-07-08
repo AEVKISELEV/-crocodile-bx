d52d72baf227d6ec971f01a57d06970c406c098e
petr <pyotr.popov443@mail.ru>
2022-07-08 22:50:12 +0200

    crocodile: initial commit

---
+++ install/components/crocodile.game/class.php
@@ -0,0 +1,40 @@
+<?php
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+{
+	die();
+}
+
+use Bitrix\Main\Loader;
+use Bitrix\Main\Localization\Loc;
+
+Loader::includeModule('crocodile');
+
+// Loc::loadMessages(__DIR__ . '/template.php');
+
+class UpTeremokTest extends CBitrixComponent
+{
+	protected function prepareResult(): array
+	{
+		return [];
+	}
+
+	protected function getApplication(): CMain
+	{
+		global $APPLICATION;
+		return $APPLICATION;
+	}
+
+	public function executeComponent()
+	{
+		$this->arResult = $this->prepareResult();
+
+		$this->includeTemplate();
+	}
+
+	protected function includeTemplate(): void
+	{
+		$this->includeComponentTemplate();
+	}
+
+}
---
+++ install/components/crocodile.game/templates/.default/template.php
@@ -0,0 +1,23 @@
+<?php
+
+/** @var array $arResult */
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+{
+	die();
+}
+
+// \Bitrix\Main\UI\Extension::load('up.ad-list');
+
+?>
+123
+<!--<div id="teremok-application"></div>-->
+<!---->
+<!--<script>-->
+<!--	BX.ready(function() {-->
+<!--		window.UpTeremokAdListApplication = new BX.Up.Teremok.AdListApplication({-->
+<!--			rootNodeId: 'teremok-application'-->
+<!--		});-->
+<!--		window.UpTeremokAdListApplication.start();-->
+<!--	})-->
+<!--</script>-->
