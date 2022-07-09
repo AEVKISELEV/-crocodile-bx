db4879d34f68e7c28cda1cfef91eb45443547d37
Artem <imbatvink@gmail.com>
2022-07-09 12:02:29 +0200

    crocodile: метод getRoom

--- lib/Controller/CrocodileController.php
+++ lib/Controller/CrocodileController.php
@@ -1,15 +1,65 @@
 <?php
 
 use Bitrix\Main;
+use Up\Bitflix\Model\RoomTable;
 
 class CrocodileController extends Main\Engine\Controller
 {
-
-	private $words = ['рекурсия'];
+	private array $words = ['рекурсия'];
 
 	public function editRoomAction(int $roomId, int $artistId, int $pictureId): void
 	{
 
 	}
+
+
+	public function getRoomAction()
+	{
+		global $USER;
+		$room = $this->getRoom();
+		$userID = $USER->GetID();
+
+		if ($room['ARTIST_ID'] !== 0)
+		{
+			$artist = $USER::GetByID($room['ID']);
+			return [
+				'idRoom' => $room['ID'],
+				'artistName' => $artist->GetFirstName(),
+				'isArtist' => false,
+				'userID' => $userID,
+			];
+		}
+
+		if($room['ARTIST_ID'] === 0)
+		{
+			$this->updateRoom($room['ID'], $userID);
+			return [
+				'idRoom' => $room['ID'],
+				'artistName' => $USER->GetFirstName(),
+				'isArtist' => true,
+				'word' => $room['word'],
+				'userID' => $userID,
+			];
+		}
+	}
+
+	private function getRoom()
+	{
+		$parameters = [
+			'select' => ['*'],
+			'LIMIT' => 1
+		];
+
+		return RoomTable::getList($parameters)->fetch();
+	}
+
+	private function updateRoom($id, $artist_id): void
+	{
+		$parameters = [
+			'ARTIST_ID' => $artist_id
+		];
+		RoomTable::update($id, $parameters);
+	}
+
 
 }
