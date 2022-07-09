95829cf7eea660fd0de266a661dc81c7ea4075af
petr <pyotr.popov443@mail.ru>
2022-07-09 09:59:03 +0200

    hack.crocodile: controller

---
+++ lib/Controller/CrocodileController.php
@@ -0,0 +1,15 @@
+<?php
+
+use Bitrix\Main;
+
+class CrocodileController extends Main\Engine\Controller
+{
+
+	private $words = ['рекурсия'];
+
+	public function editRoomAction(int $roomId, int $artistId, int $pictureId): void
+	{
+
+	}
+
+}
